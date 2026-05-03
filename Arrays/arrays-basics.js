e3a4818f4a0f78d16047f33664bcf3d6c6553acb
amankhan790 <amankassar790@gmail.com>
2026-05-03 11:00:02 +0530

    second largest element

--- Arrays/arrays-basics.js
+++ Arrays/arrays-basics.js
@@ -1,123 +1,157 @@
-// Arrays in Javascript
+// // Arrays in Javascript
 
-// Decalaration
-let person = {
-    name: "Piyush",
-    age: 25,
-};
+// // Decalaration
+// let person = {
+//     name: "Piyush",
+//     age: 25,
+// };
 
-let arr = ["apple", "banana", "cherry", person];
+// let arr = ["apple", "banana", "cherry", person];
 
-console.log(arr.length); // know the length of an array
+// console.log(arr.length); // know the length of an array
 
-// # Add and Remove Elements
+// // # Add and Remove Elements
 
-// Add to End of the Array
-arr.push("orange");
+// // Add to End of the Array
+// arr.push("orange");
 
-// Remove From End of the Array
-arr.pop();
-arr.pop();
+// // Remove From End of the Array
+// arr.pop();
+// arr.pop();
 
-// Add to Top of the Array
-arr.unshift("orange");
+// // Add to Top of the Array
+// arr.unshift("orange");
 
-// Remove From Top of the Array
-arr.shift();
+// // Remove From Top of the Array
+// arr.shift();
 
-// # Looping Arrays
+// // # Looping Arrays
 
-// For Loop
-for (let i = 0; i < arr.length; i++) {
-    // console.log(arr[i]);
+// // For Loop
+// for (let i = 0; i < arr.length; i++) {
+//     // console.log(arr[i]);
+// }
+
+// // While Loop
+// let i = 0;
+// while (i < arr.length) {
+//     // console.log(arr[i]);
+
+//     i++;
+// }
+
+// // # Inbuilt Loop Methods
+// const numbers = [1, 2, 3, 4, 5];
+
+// // map in javascript
+// numbers.map((item, index, array) => {
+//     return item + 5;
+// });
+
+// // filter in javascript
+// numbers.filter((item, index, array) => {
+//     return item > 3;
+// });
+
+// // reduce in javascript
+// numbers.reduce((prev, item) => {
+//     return prev + item;
+// }, 2);
+
+// // some in javascript
+// numbers.some((item, index, array) => {
+//     return item > 3;
+// });
+
+// // every in javascript
+// numbers.every((item, index, array) => {
+//     return item < 10;
+// });
+
+// // find in javascript
+// numbers.find((item, index, array) => {
+//     return item > 3;
+// });
+
+// // findIndex
+// const index = [1, 2, 3].findIndex((item) => item === 2);
+// // console.log(index);
+
+// // Spread and Rest Operators
+// const nums = [1, 2, 3];
+// const nums2 = [4, 5, 6, 7];
+
+// const finalNums = [...nums, ...nums2]; // Spread Operator
+
+// // Rest Operator
+// function sum(...numbers) {
+//     return numbers;
+// }
+
+// console.log(sum(nums, nums2, 5, "hello"));
+
+// // # More Array Methods
+
+// // concat
+// nums.concat(nums2, arr);
+
+// // Slice
+// arr.slice(-2);
+
+// // Splice
+// arr.splice(1, 2, "orange");
+
+// // Fill
+// const dummy = [2, 5, 1, 6];
+// dummy.fill(2, 2);
+
+// // Flat
+// const flatEx = [1, [2, 3], [[4, 5], 6]]; // => [1,2,3,4,5,6]
+
+// const flattenedArray = flatEx.flat(2);
+// // console.log(flattenedArray);
+
+// // reverse
+// nums.reverse();
+// // console.log(nums);
+
+// // sort
+// const unsorted = [5, 2, 10, 7, 3, 1];
+// unsorted.sort((a, b) => b - a);
+// console.log(unsorted);
+
+
+// let nums = [56, 74, 33, 56, 88, 3, 6,]
+
+// function secondLargest(nums) {
+
+//     const newAray = Array.from(new Set(nums))
+//     let res = newAray.sort((a, b) => b - a)
+//     if (res.length >= 2) {
+//         return res[1]
+//     } else {
+//         return -1
+//     }
+// }
+
+// console.log(secondLargest([5, 10, 10])); 
+
+
+
+function secondLargest(nums) {
+    let largest = -1 // 56 88
+    let secondLargest = -1  //-1 74
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > largest) {
+            secondLargest = largest
+            largest = nums[i]
+        } else if (nums[i] != largest && nums[i] > secondLargest) {
+            secondLargest = nums[i]
+
+        }
+    }
+    return secondLargest
 }
 
-// While Loop
-let i = 0;
-while (i < arr.length) {
-    // console.log(arr[i]);
-
-    i++;
-}
-
-// # Inbuilt Loop Methods
-const numbers = [1, 2, 3, 4, 5];
-
-// map in javascript
-numbers.map((item, index, array) => {
-    return item + 5;
-});
-
-// filter in javascript
-numbers.filter((item, index, array) => {
-    return item > 3;
-});
-
-// reduce in javascript
-numbers.reduce((prev, item) => {
-    return prev + item;
-}, 2);
-
-// some in javascript
-numbers.some((item, index, array) => {
-    return item > 3;
-});
-
-// every in javascript
-numbers.every((item, index, array) => {
-    return item < 10;
-});
-
-// find in javascript
-numbers.find((item, index, array) => {
-    return item > 3;
-});
-
-// findIndex
-const index = [1, 2, 3].findIndex((item) => item === 2);
-// console.log(index);
-
-// Spread and Rest Operators
-const nums = [1, 2, 3];
-const nums2 = [4, 5, 6, 7];
-
-const finalNums = [...nums, ...nums2]; // Spread Operator
-
-// Rest Operator
-function sum(...numbers) {
-    return numbers;
-}
-
-console.log(sum(nums, nums2, 5, "hello"));
-
-// # More Array Methods
-
-// concat
-nums.concat(nums2, arr);
-
-// Slice
-arr.slice(-2);
-
-// Splice
-arr.splice(1, 2, "orange");
-
-// Fill
-const dummy = [2, 5, 1, 6];
-dummy.fill(2, 2);
-
-// Flat
-const flatEx = [1, [2, 3], [[4, 5], 6]]; // => [1,2,3,4,5,6]
-
-const flattenedArray = flatEx.flat(2);
-// console.log(flattenedArray);
-
-// reverse
-nums.reverse();
-// console.log(nums);
-
-// sort
-const unsorted = [5, 2, 10, 7, 3, 1];
-unsorted.sort((a, b) => b - a);
-console.log(unsorted);
-
-
+console.log(secondLargest([56, 74, 33, 56, 88, 3, 6])); 
